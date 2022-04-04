250518950b6d3ca01304da14210e2e0c8fdc4b3f
aniket2407 <jainani2407@gmail.com>
2022-04-04 15:07:09 +0530

    Initial commit

---
+++ src/QuickSort.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class QuickSort {
+    // Swap function
+    static void swap(int arr[],int i,int j)
+    {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    //
+    static int partition(int arr[],int beg, int end)
+    {
+        int pivot = arr[end];
+        int i = (beg-1);
+
+        for (int j = beg; j <= end; j++) {
+            if(arr[j]<pivot)
+            {
+                i++;
+                swap(arr,i,j);
+            }
+
+        }
+        swap(arr,i+1,end);
+        return i+1;
+    }
+
+    static void quicksort(int arr[],int beg, int end)
+    {
+        if(beg<end)
+        {
+            int split = partition(arr,beg,end);
+            quicksort(arr,beg,split-1);
+            quicksort(arr,split+1,end);
+        }
+    }
+
+    static void Print(int arr[])
+    {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        quicksort(arr,0,n-1);
+        Print(arr);
+
+    }
+}
